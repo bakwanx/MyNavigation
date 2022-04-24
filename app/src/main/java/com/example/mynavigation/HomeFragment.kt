eebd80969f1d2d194103d78ca5fafa462c72cb37
farhan <farhanfadhilah87@gmail.com>
2022-04-24 19:09:02 +0700

    send data using bundle

--- app/src/main/java/com/example/mynavigation/HomeFragment.kt
+++ app/src/main/java/com/example/mynavigation/HomeFragment.kt
@@ -24,7 +24,6 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.btnCategory.setOnClickListener(
             //to fragment
             Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
